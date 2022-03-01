dccf06ca0b4592b53e0e5b5edf5d6cfd2012da20
Wael Ben Dhia <wael.bendhia@prosperus.tech>
2022-03-01 15:53:52 +0100

    make /main.go generate nyms

--- main.go
+++ main.go
@@ -1,34 +1,12 @@
 package main
 
 import (
-	"database/sql"
-	"log"
-
-	"git.digitus.me/pfe/smart-wallet/api"
-	db "git.digitus.me/pfe/smart-wallet/db/sqlc"
-	"git.digitus.me/pfe/smart-wallet/util"
-	_ "github.com/lib/pq"
+	"fmt"
+	"git.digitus.me/prosperus/protocol/identity"
 )
 
 func main() {
-	config, err := util.LoadConfig(".")
-	if err != nil {
-		log.Fatal("cannot load config", err)
-	}
-	conn, err := sql.Open(config.DBDriver, config.DBSource)
-
-	if err != nil {
-		log.Fatal("cannot connect to db:", err)
-	}
-
-	store := db.NewStore(conn)
-	server, err := api.NewServer(config, store)
-	if err != nil {
-		log.Fatal("cannot create server :", err)
-	}
-
-	err = server.Start(config.ServerAddress)
-	if err != nil {
-		log.Fatal("cannot start server:", err)
+	for i := 0; i < 20; i++ {
+		fmt.Println(identity.NewSecretKey().PublicKey())
 	}
 }
