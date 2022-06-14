777eaed2cbfd6ce3b7f18216349ac76cec7538e4
Wael Ben Dhia <wael.bendhia@prosperus.tech>
2022-06-14 14:15:11 +0100

    review

--- cmd/Routine-Transaction-Policy-Schedueler/main.go
+++ cmd/Routine-Transaction-Policy-Schedueler/main.go
@@ -8,80 +8,117 @@ import (
 	"sync"
 	"time"
 
+	"git.digitus.me/library/prosper-kit/application"
 	"git.digitus.me/pfe/smart-wallet/types"
 	"git.digitus.me/prosperus/publisher"
 	"github.com/nsqio/go-nsq"
 	"github.com/robfig/cron/v3"
 )
 
+// This should be in its own package
 type inMemory struct {
 	routinePolicy map[int]cron.EntryID
+	mu            sync.Mutex
+}
+
+type RTPTicker interface {
+	HandleNewPolicy(context.Context, types.RoutineTransactionPolicy) error
+	HandleDeletePolicy(context.Context, int) error
+}
+
+var _ (RTPTicker) = (*inMemory)(nil)
+
+func HandleRTPMessages(rtpTicker RTPTicker) func(context.Context, *nsq.Message) error {
+	return func(ctx context.Context, msg *nsq.Message) error {
+    var type string
+    // here we deserialize and hande to the approriate method
+    switch type {
+    case "NEW":
+      return rtpTrtpTicker.HandleNewPolicy(ctx, types.RoutineTransactionPolicy{})
+    case "DELETE":
+      return rtpTrtpTicker.HandleDeletePolicy(ctx, 0)
+    default:
+      // log a warning about unknown type
+      return nil
+    }
+	}
 }
 
 var inMemoryRoutinePolicy = &inMemory{routinePolicy: make(map[int]cron.EntryID)}
-var mu sync.Mutex
 
 func runCronJobs(s *cron.Cron, rtp types.RoutineTransactionPolicy) {
 	var frequency = "every 5s"
+
 	entryId, err := s.AddFunc("@"+frequency, func() {
 		if time.Now().After(rtp.ScheduleStartDate) && time.Now().Before(rtp.ScheduleEndDate) {
+			// never use fmt package for logging use logger from context with prospercontext.GetLogger
 			fmt.Println("Every hour on the half hour")
 		}
 	})
 	if err != nil {
+		// never use log package for logging use logger from context with prospercontext.GetLogger
 		log.Println(err)
 	}
+
 	entry := s.Entry(entryId)
+	// never use fmt package for logging use logger from context with prospercontext.GetLogger
 	fmt.Println(entry)
-	mu.Lock()
-	inMemoryRoutinePolicy.routinePolicy[rtp.ID] = entryId
-	mu.Unlock()
+	func() {
+		inMemoryRoutinePolicy.mu.Lock()
+		defer inMemoryRoutinePolicy.mu.Unlock()
+		inMemoryRoutinePolicy.routinePolicy[rtp.ID] = entryId
+	}()
+	// never use fmt package for logging use logger from context with prospercontext.GetLogger
 	fmt.Println(inMemoryRoutinePolicy.routinePolicy)
+	// never use fmt package for logging use logger from context with prospercontext.GetLogger
 	fmt.Println(rtp.RequestType)
 	s.Start()
 }
 
-func main() {
-
+func run(ctx context.Context) error {
 	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	s := cron.New()
-	var data types.RoutineTransactionPolicy
-	config := publisher.NewNSQConfig()
-	c, err := publisher.NewConsumer(context.Background(), "Add-Routine-Transaction-Policy", "Rtp", config)
-	if err != nil {
-		return
-	}
-	c.AddHandler(func(ctx context.Context, m *nsq.Message) error {
-		err := json.Unmarshal(m.Body, &data)
-		if err != nil {
-			return fmt.Errorf("error %w", err)
-		}
-		switch data.RequestType {
-		case "POST":
-			runCronJobs(s, data)
-		case "DELETE":
-			if entryId, ok := inMemoryRoutinePolicy.routinePolicy[data.ID]; ok {
-				s.Remove(entryId)
-				delete(inMemoryRoutinePolicy.routinePolicy, data.ID)
-				fmt.Println(inMemoryRoutinePolicy.routinePolicy)
-			}
-		case "PUT":
-			if entryId, ok := inMemoryRoutinePolicy.routinePolicy[data.ID]; ok {
-				s.Remove(entryId)
-				delete(inMemoryRoutinePolicy.routinePolicy, data.ID)
-				fmt.Println(inMemoryRoutinePolicy.routinePolicy)
-				runCronJobs(s, data)
-			}
-		}
-		log.Println(data)
-		return nil
-	})
+	defer wg.Wait()
 
-	err = c.ConnectToNSQD("127.0.0.1:4150")
+	s := cron.New()
+
+	wg.Add(1)
+	s.Start()
+
+	go func() {
+		<-ctx.Done()
+		s.Stop()
+	}()
+
+	defer func() {
+		defer wg.Done()
+		<-s.Stop().Done()
+	}()
+
+	config := publisher.NewNSQConfig()
+	c, err := publisher.NewConsumer(
+		context.Background(), "routine-transaction-policies", "ticker", config,
+	)
 	if err != nil {
-		log.Panic("Could not connect")
+		return err
 	}
-	wg.Wait()
-	fmt.Scanln()
+
+  var rtpTicker RTPTicker 
+  {
+    // Here we initialize the ticker
+  }
+
+	// handler func should be a method on inMemory
+	c.AddHandler(HandleRTPMessages(rtp))
+
+	// Should be read from config
+	err = c.ConnectToNSQLookupd("127.0.0.1:4150")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	application.WaitForInterrupt(run)
 }
---
+++ cmd/trigger-handler/main.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	// This is where we read events and launch transactions according to policies
+}
